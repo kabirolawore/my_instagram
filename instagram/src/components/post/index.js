dec93088dceea1dbc926dade8e5c6d71d13c3713
kabirolawore <horlaworey@gmail.com>
2023-07-20 16:36:31 +0100

    post component added

---
+++ instagram/src/components/post/index.js
@@ -0,0 +1,26 @@
+import { useRef } from 'react';
+import PropTypes from 'prop-types';
+import Header from './Header';
+
+function Post({ content }) {
+  return (
+    <div>
+      <Header username={content.username} />
+    </div>
+  );
+}
+
+export default Post;
+
+Post.propTypes = {
+  content: PropTypes.shape({
+    username: PropTypes.string.isRequired,
+    imageSrc: PropTypes.string.isRequired,
+    caption: PropTypes.string.isRequired,
+    docId: PropTypes.string.isRequired,
+    userLikedPhoto: PropTypes.bool.isRequired,
+    likes: PropTypes.array.isRequired,
+    comments: PropTypes.array.isRequired,
+    dateCreated: PropTypes.number.isRequired,
+  }),
+};
